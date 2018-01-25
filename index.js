6032d424fdb6c7d1c3885b2bb02357a8d54b5545
Daniel Dunbar <danielqdunbar@gmail.com>
2018-01-25 02:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-var animal = 'dog'
+var myAnimal = 'dog'
+var yourAnimal = 'cat'
 
 function myAnimal() {
+<<<<<<< HEAD
   animal = 'dog'
   return animal
+=======
+  return myAnimal
+>>>>>>> 35662fe62547da9e13ef199662a8ca9957adcd3e
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+<<<<<<< HEAD
   animal = 'cat'
   return animal
+=======
+  return yourAnimal
+>>>>>>> 35662fe62547da9e13ef199662a8ca9957adcd3e
 }
 
 function add2(n) {
